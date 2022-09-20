da7bca94fa1310c6393e70dcb5d4bf3ac9041001
Luis Polina <luis.282001@gmail.com>
2022-09-20 09:44:07 -0500

    start button changes

--- src/app/game/screen/screen.component.ts
+++ src/app/game/screen/screen.component.ts
@@ -11,7 +11,7 @@ export class ScreenComponent implements OnInit {
   @ViewChild('board') board!: ElementRef<HTMLDivElement>;
 
 
-  state: boolean = false;
+  state = new Array(9).fill(false);
   finished: boolean = true;
   symbol: number = 0;
 
@@ -24,36 +24,34 @@ export class ScreenComponent implements OnInit {
   }
 
   Start() {
-    this.state = true;
+
+    for (let i=0; i<9; i++) {
+      this.board.nativeElement.children[i].textContent = '';
+    }
+      
+
+    this.state.forEach( (_,i) => {
+      this.state[i] = true;
+    })
     this.finished = false;
 
   }
 
-  Set( square: string) {
+  Set( square: number) {
 
     if( this.symbol === 0 ){
-
-      switch(square){
-        case('s1'):
-          this.board.nativeElement.children[0].textContent = 'X';
-          break
-
-          
-          
-      }
+      this.board.nativeElement.children[square].textContent = 'X';
+      this.board.nativeElement.children[square].classList.add('ejemplo');
+      this.state[square] = false;
       this.symbol = 1; 
     }
-    else {
-      
-      switch(square){
-        case('s1'):
-        this.board.nativeElement.children[0].textContent = 'O';
-          break
-       
-      }
 
+    else {
+      this.board.nativeElement.children[square].textContent = 'O';
+      this.state[square] = false;
       this.symbol = 0;
     }
   }
+
 
 }
