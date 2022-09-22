a76b0c7d44866ba72c759be1f63dcf848aa9c535
P4R-DARK <luis.282001@gmail.com>
2022-09-22 17:34:11 -0500

    wins and draw

--- src/app/game/screen/screen.component.ts
+++ src/app/game/screen/screen.component.ts
@@ -14,6 +14,8 @@ export class ScreenComponent implements OnInit {
   state = new Array(9).fill(false);
   finished: boolean = true;
   symbol: number = 0;
+  winner: string = '';
+  drawv: boolean = false;
 
   constructor() {
     
@@ -23,16 +25,19 @@ export class ScreenComponent implements OnInit {
     
   }
 
+  
+
   Start() {
+
+    this.winner = '';
 
     for (let i=0; i<9; i++) {
       this.board.nativeElement.children[i].textContent = '';
     }
       
-
     this.state.forEach( (_,i) => {
       this.state[i] = true;
-    })
+    });
     this.finished = false;
 
   }
@@ -41,7 +46,6 @@ export class ScreenComponent implements OnInit {
 
     if( this.symbol === 0 ){
       this.board.nativeElement.children[square].textContent = 'X';
-      this.board.nativeElement.children[square].classList.add('ejemplo');
       this.state[square] = false;
       this.symbol = 1; 
     }
@@ -53,5 +57,92 @@ export class ScreenComponent implements OnInit {
     }
   }
 
+  
+  Check() {
+    const first = this.board.nativeElement.firstChild!.textContent;
+    const last = this.board.nativeElement.lastChild!.textContent;
+    
+    //  Horizontal
+    if(first != '' && first === this.board.nativeElement.children[1].textContent
+      && first === this.board.nativeElement.children[2].textContent) {
+        
+        this.GameFinished(first!);
+    }
+
+    if(this.board.nativeElement.children[3].textContent !== ''
+      && this.board.nativeElement.children[3].textContent === this.board.nativeElement.children[4].textContent
+      && this.board.nativeElement.children[3].textContent === this.board.nativeElement.children[5].textContent) {
+        
+        this.GameFinished(this.board.nativeElement.children[3].textContent!);
+    }
+
+    if(this.board.nativeElement.children[6].textContent !== ''
+      && this.board.nativeElement.children[6].textContent === this.board.nativeElement.children[7].textContent
+      && this.board.nativeElement.children[6].textContent === last) {
+        
+        this.GameFinished(this.board.nativeElement.children[6].textContent!);
+    }
+    
+    //  Vertical
+    if(first !== ''
+      && first === this.board.nativeElement.children[3].textContent
+      && first === this.board.nativeElement.children[6].textContent) {
+        
+        this.GameFinished(first!);
+    }
+    
+    if(this.board.nativeElement.children[1].textContent !== ''
+      && this.board.nativeElement.children[1].textContent === this.board.nativeElement.children[4].textContent
+      && this.board.nativeElement.children[1].textContent === this.board.nativeElement.children[7].textContent) {
+        
+        this.GameFinished(this.board.nativeElement.children[1].textContent!);
+    }
+    
+    if(this.board.nativeElement.children[2].textContent !== ''
+      && this.board.nativeElement.children[2].textContent === this.board.nativeElement.children[5].textContent
+      && this.board.nativeElement.children[2].textContent === last) {
+        
+        this.GameFinished(this.board.nativeElement.children[2].textContent!);
+    }
+
+    //  Diagonal
+    if(first !== ''
+      && first === this.board.nativeElement.children[4].textContent
+      && first === last) {
+        
+        this.GameFinished(first!);
+    }
+    
+    if(this.board.nativeElement.children[2].textContent !== ''
+      && this.board.nativeElement.children[2].textContent === this.board.nativeElement.children[4].textContent
+      && this.board.nativeElement.children[2].textContent === this.board.nativeElement.children[6].textContent) {
+        this.GameFinished(this.board.nativeElement.children[2].textContent!);
+    }
+    
+
+    //  Draw
+    for (let i=0; i<9; i++) {
+      if(this.board.nativeElement.children[i].textContent === ''){
+        this.drawv = false;
+        break
+      }
+
+      this.drawv = true;
+    }
+
+    if(this.drawv)
+      this.GameFinished('');
+
+  }
+
+  GameFinished(w: string) {
+
+    this.state.forEach( (_,i) => {
+      this.state[i] = false;
+    });
+    this.winner = w;
+    this.finished = true;
+    
+  }
 
 }
